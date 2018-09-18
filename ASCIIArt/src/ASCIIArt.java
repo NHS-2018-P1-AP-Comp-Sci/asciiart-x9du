18c1174b79e8769cc12424161764a5f61554d2f0
s-zouci <s-zouci@53S50030645.it.bsd405.org>
2018-09-18 08:48:25 -0700

    Cindy Zou's Project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,17 +1,43 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
 
-// YOUR CODE GOES HERE :)
+public class ASCIIArt {
+
+	public static void main(String[] args) {
+		fourHeads();
+	}
+	
+	public static void topBottom() {
+		System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+	}
+	
+	public static void border() {
+		topBottom();
+		topBottom();
+	}
+	
+	public static void twoHeads() {
+		System.out.println("|||||||||||,-`\"\"‾‾‾\"\"-||||||||||||||||||||||,-`\"\"‾‾‾\"\"-||||||||||");
+		System.out.println("||||||||ƴ\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\|||||||||||||||||ƴ\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\||||||||");
+		System.out.println("|||||||/\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\|||||||||||||||/\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\|||||||");
+		System.out.println("||||||/\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"۱|||||||||||||/\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"۱||||||");
+		System.out.println("||||||\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|||||||||||||\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|||||||");
+		System.out.println("||||||\"\"    \"\",\"\"     \"\"\"\"-|||||||||||\"\"    \"\",\"\"     \"\"\"\"-||||||");
+		System.out.println("||||||\\\"                 | ]||||||||||\\\"                 | ]|||||");
+		System.out.println("|||||||                    ||||||||||||                    ||||||");
+		System.out.println("|||||||۱                  |||||||||||||۱                  |||||||");
+		System.out.println("||||||||\\,_        _,-'  /||||||||||||||\\,_        _,-'  /|||||||");
+		System.out.println("|||||||||[ ‾͝‾‾    ҃‾‾҃    |||||||||||||||||[ ‾͝‾‾    ҃‾‾҃    |||||||||");
+		System.out.println("||||||||||\\  >    ,    / \\||||||||||||||||\\  >    ,    / \\|||||||");
+		System.out.println("|||||||||||\\  \\_/‾    /   \\||||||||||||||||\\  \\_/‾    /   \\||||||");
+		System.out.println("|||||||||||||\\‾`͡͝`҃``  /    /||||||||||||||||||\\‾`͡͝`҃``  /    /||||||");
+		System.out.println("|||||||||||||||`---/...--҃||||||||||||||||||||||`---/...--҃||||||||");
+	}
+	
+	public static void fourHeads() {
+		border();
+		twoHeads();
+		border();
+		twoHeads();
+		border();
+	}
+
+}
